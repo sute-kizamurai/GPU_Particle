e18cd4908a3a0314b75298f6e92676749f6a86e6
sute-kizamurai <nakayama.hayate4510471@gmail.com>
2025-01-23 10:33:27 +0900

    パーティクル初期化用コンピュートシェーダに初期化するコードを書き込む

--- shader/particleInitialCS.hlsl
+++ shader/particleInitialCS.hlsl
@@ -25,5 +25,21 @@ RWStructuredBuffer<PARTICLE_LOCAL_CONFIG> BufOut : register(u0);
 [numthreads(size_x, size_y, size_z)]
 void main(const CSInput input)
 {
+    //ディスパッチ内での一意の値を格納
     int index = input.dispatch.x;
+    
+    //位置の初期化
+    BufOut[index].Position = float3(0.0, 0.0, 0.0);
+    
+    //発射方向の乱数を作成
+    
+    //速度を初期化
+    BufOut[index].Velocity = float3(0.0, 1.0, 0.0) * ParticleGlobalConfig.SpeedFactor;
+    
+    //加速度を初期化
+    BufOut[index].Acceleration = float3(0.0, 0.0, 0.0);
+    
+    //ライフを初期化
+    BufOut[index].Life = ParticleGlobalConfig.MaxLife;
+
 }
