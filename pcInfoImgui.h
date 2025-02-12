5e7f954303e1c561c34a8a96e428e93f066b4951
sute-kizamurai <nakayama.hayate4510471@gmail.com>
2025-02-12 17:44:33 +0900

    一旦保存

--- pcInfoImgui.h
+++ pcInfoImgui.h
@@ -6,11 +6,21 @@
 #include "imguiBase.h"
 #include "gameObject.h"
 
+////構造体宣言
+struct PC_INFOMATION
+{
+	float Fps;
+
+};
+
 class PcInfoImgui : public GameObject
 {
 private:
 	//パーティクル用のImGuiを格納する変数
 	std::vector<ImguiBase*> m_Element;
+
+	//コンピュートシェーダにPCの基本情報を送信するための構造体
+	PC_INFOMATION* m_PcInfomation;
 
 public:
 	void Init() override;
