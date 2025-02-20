42f44f0916dc94b6776055e2ec64650e4e453cf9
sute-kizamurai <nakayama.hayate4510471@gmail.com>
2025-02-20 15:08:55 +0900

    一旦セーブ

--- pcFpsImgui.cpp
+++ pcFpsImgui.cpp
@@ -8,9 +8,19 @@ void PcFpsImgui::Init()
 
 void PcFpsImgui::Draw()
 {
-	//PCのFPSを表示
-	ImGui::Text("FPS : %.2f", GetFps());
+	//PCのFPS&デルタタイムを表示
+	ImGui::Text("%.3f ms", ImGui::GetIO().DeltaTime * 1000.0f);
+	ImGui::SameLine();
+	ImGui::Text("FPS : %.1f", ImGui::GetIO().Framerate);
+
+	static float value[180];
+	for (int i = 0; i < 179; i++)
+		value[i] = value[i + 1];
+
+	value[179] = ImGui::GetIO().DeltaTime * 1000.0f;
+
+	ImGui::PlotLines("GraphDeltaTime", value, sizeof(value) / sizeof(float), 0, NULL, 0.0f, 100.0f, ImVec2(0, 50));
 
 	//PCのFPSをセット
-	m_PcInfoImgui->SetFps(GetFps());
+	m_PcInfoImgui->SetFps(ImGui::GetIO().Framerate);
 }
