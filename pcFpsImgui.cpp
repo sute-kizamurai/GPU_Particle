597f55beb936e72d256bc2bcc8d0da60ed4ca187
sute-kizamurai <nakayama.hayate4510471@gmail.com>
2025-02-20 11:28:10 +0900

    DeltaTimeをPCinfoに表示するように変更 それに伴いPCinfoのImGUIの大きさを変更

--- pcFpsImgui.cpp
+++ pcFpsImgui.cpp
@@ -11,6 +11,9 @@ void PcFpsImgui::Draw()
 	//PCのFPSを表示
 	ImGui::Text("FPS : %.2f", ImGui::GetIO().Framerate);
 
+	//DeltaTimeを表示
+	ImGui::Text("DeltaTime : %.2fms", ImGui::GetIO().DeltaTime * 1000.0f);
+
 	//PCのFPSをセット
 	m_PcInfoImgui->SetFps(ImGui::GetIO().Framerate);
 }
