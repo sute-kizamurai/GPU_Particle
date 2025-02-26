b7405c311ba7bebe864b08002a93cd7d2f167845
sute-kizamurai <nakayama.hayate4510471@gmail.com>
2025-02-26 10:12:12 +0900

    一旦フルスクリーンを解除

--- pcFpsImgui.cpp
+++ pcFpsImgui.cpp
@@ -9,7 +9,7 @@ void PcFpsImgui::Init()
 void PcFpsImgui::Draw()
 {
 	//PCのFPS&デルタタイムを表示
-	ImGui::Text("%.3f ms", ImGui::GetIO().DeltaTime * 1000.0f);
+	ImGui::Text("DeltaTime : %.3f ms", ImGui::GetIO().DeltaTime * 1000.0f);
 	ImGui::SameLine();
 	ImGui::Text("FPS : %.1f", ImGui::GetIO().Framerate);
 
@@ -20,23 +20,6 @@ void PcFpsImgui::Draw()
 	value[179] = ImGui::GetIO().DeltaTime * 1000.0f;
 
 	ImGui::PlotLines("GraphDeltaTime", value, sizeof(value) / sizeof(float), 0, NULL, 0.0f, 100.0f, ImVec2(0, 50));
-	//PCのFPSを表示
-	ImGui::Text("FPS : %.2f", ImGui::GetIO().Framerate);
-
-	//DeltaTimeを表示
-	ImGui::Text("DeltaTime : %.2fms", ImGui::GetIO().DeltaTime * 1000.0f);
-
-
-	//DeltaTimeの変化を表すグラフ用の配列を更新
-	for (int i = 0; i < 17; i++)
-	{
-		m_DeltaTimeGraph[i] = m_DeltaTimeGraph[i + 1];
-	}
-	m_DeltaTimeGraph[17] = ImGui::GetIO().DeltaTime;
-
-	//DeltaTimeのグラフを作成
-	ImGui::PlotLines("DeltaTimeGraph", m_DeltaTimeGraph, IM_ARRAYSIZE(m_DeltaTimeGraph));
-
 
 	//PCのFPSをセット
 	m_PcInfoImgui->SetFps(ImGui::GetIO().Framerate);
